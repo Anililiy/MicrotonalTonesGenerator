4f2aa4170bf68aee1bb69ebf38131616a875ffea
Anna <Annayamsh@yahoo.co.uk>
2014-10-30 00:11:56 +0000

    Made changes to storyboard
    Table view works better

--- MTGFirstLoginViewController.m
+++ MTGFirstLoginViewController.m
@@ -54,7 +54,7 @@
         [defaults setFloat:colourSat forKey:@"initThemeSat"];
         [defaults setFloat:colourBrg forKey:@"initThemeBrg"];
         
-        if (![defaults objectForKey:@"loggedIn"] || ![defaults boolForKey:@"loggedIn"]) [defaults setBool:YES forKey:@"loggedIn"];
+        if (![defaults objectForKey:@"firstRun"] || ![defaults boolForKey:@"firstRun"]) [defaults setBool:YES forKey:@"firstRun"];
         
         [defaults synchronize];
         
